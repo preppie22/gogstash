00e99bcdd3c31022fdd374852cf6d883ff740432
Piyush Puranik <piyushpuranik@gmail.com>
2026-09-12 10:26:01 -0700

    Add manifest handling to record downloaded content

---
+++ src/gogstash/manifest.py
@@ -0,0 +1,39 @@
+import json
+from pathlib import Path
+import time
+
+
+MANIFEST_FILE = ".gogstash.manifest"
+
+def _get_manifest(game_dir: Path) -> dict:
+    manifest_file = Path(game_dir) / MANIFEST_FILE
+    manifest = None
+    try:
+        with open(manifest_file, 'r') as rp:
+            manifest = json.load(rp)
+    except FileNotFoundError:
+        return {'error': f'Missing: {str(manifest_file)}'}
+    return manifest
+
+def add_file(game_dir: Path, filepath: Path, checksum: str, timestamp: float) -> None:
+    manifest = _get_manifest(game_dir)
+    if 'error' in manifest:
+        manifest = {}
+    filepath_abs = Path(game_dir) / Path(filepath)
+    if not filepath_abs.exists():
+        raise FileNotFoundError(f"No such file {filepath_abs}")
+    manifest[str(filepath)] = {
+        'size': filepath_abs.stat().st_size,
+        'checksum': checksum,
+        'fetched_at': timestamp
+    }
+    temp_file = Path(game_dir) / f"{MANIFEST_FILE}~"
+    with open(temp_file, 'w') as wp:
+        json.dump(manifest, wp)
+    temp_file.replace(Path(game_dir) / Path(MANIFEST_FILE))
+
+def stat_file(game_dir: Path, filepath: Path) -> dict:
+    manifest = _get_manifest(game_dir)
+    if 'error' in manifest:
+        return {}
+    return manifest.get(str(filepath), {})
